f1ef2a34df5f9c74eb44711008aa02789ffcd88d
Timo Henderson <46020614+TimoHenderson@users.noreply.github.com>
2023-05-31 01:48:47 +0100

    tidied up

--- recording-tracker-react/src/components/Widgets/CompletionMeter.js
+++ recording-tracker-react/src/components/Widgets/CompletionMeter.js
@@ -5,28 +5,16 @@ const CompletionMeter = ({ childElements }) => {
     const meterNodes = childElements ? childElements.map((element) => {
         const completion = element.completion;
         return (
-
-
             <Box sx={{ display: "flex", flexDirection: "column", height: "100%", width: "100%" }}>
                 <Box height={`${100 - completion}%`} key={element.id} sx={{ background: "#00FF0044", width: "100%" }}></Box>
                 <Box height={`${completion}%`} key={element.id} sx={{ background: "#00DD00", width: "100%" }}></Box>
             </Box>
-
-
         );
     }) : null;
     console.log(meterNodes);
     return (
         <Box sx={{ display: "flex", alignItems: "flex-start", gap: "2px", background: "black", width: "100%", padding: "2px" }}>
-            {/* <Stack
-                direction="row"
-                justifyContent="center"
-                alignItems="flex-end"
-                spacing={2}
-                width="100%"
-            > */}
             {meterNodes}
-            {/* </Stack> */}
         </Box>
     );
 }
